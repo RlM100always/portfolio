6aa11b9c16a91c47a1875b9e2666e6128a76b544
RlM100always <rakib100rlms@gmail.com>
2025-05-25 13:56:08 +0600

    portfolio app

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/', // 👈 this must match your repo name
+  base: '/portfolio/', // 👈 IMPORTANT: matches your repo name!
 })
